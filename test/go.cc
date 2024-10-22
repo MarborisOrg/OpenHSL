2388dc288cf723c5aefd11ec2fb00af74a37cf64
MRB <mehrabshafae@gmail.com>
2024-10-23 02:21:10 +0330

    mrb: add jthread for test

---
+++ test/go.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+class jthreadTaskClass
+{
+public:
+    template <typename Func>
+    jthreadTaskClass &operator>>(Func &&func)
+    {
+        std::jthread j_thread(std::forward<Func>(func));
+        return *this;
+    }
+
+    template <typename Func>
+    jthreadTaskClass &operator<<(Func &&func)
+    {
+        std::jthread([func = std::forward<Func>(func)](std::stop_token st)
+                                         {
+            if (st.stop_requested()) {
+                return;
+            }
+            func(); }).detach();
+        return *this;
+    }
+};
+
+jthreadTaskClass go;
+
+int main()
+{
+    go >> []() {
+        go << []()
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            std::cout << "Function a is running\n";
+        }
+        << []()
+        {
+            std::cout << "Function b is running\n";
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+        }
+        << []()
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+            std::cout << "Function c is running\n";
+        }
+        << []()
+        {
+            std::cout << "Function d is running\n";
+        };
+    };
+
+    go >> []()
+    {
+        std::cout << "Function e is running\n";
+    };
+
+    return 0;
+}
