eaceb207aab92785ef42120cf4151ecc4ba3a9d9
MRB <mehrabshafae@gmail.com>
2024-10-23 10:01:12 +0330

    mrb: jthread(test)

--- test/go.cc
+++ test/go.cc
@@ -7,21 +7,9 @@ class jthreadTaskClass
 {
 public:
     template <typename Func>
-    jthreadTaskClass &operator>>(Func &&func)
-    {
-        std::jthread j_thread(std::forward<Func>(func));
-        return *this;
-    }
-
-    template <typename Func>
     jthreadTaskClass &operator<<(Func &&func)
     {
-        std::jthread([func = std::forward<Func>(func)](std::stop_token st)
-                                         {
-            if (st.stop_requested()) {
-                return;
-            }
-            func(); }).detach();
+        std::jthread(std::forward<Func>(func)).detach();
         return *this;
     }
 };
@@ -30,32 +18,32 @@ jthreadTaskClass go;
 
 int main()
 {
-    go >> []() {
+    go << []()
+    {
         go << []()
         {
             std::this_thread::sleep_for(std::chrono::seconds(2));
             std::cout << "Function a is running\n";
-        }
-        << []()
+        } << []()
         {
             std::cout << "Function b is running\n";
             std::this_thread::sleep_for(std::chrono::seconds(5));
-        }
-        << []()
+        } << []()
         {
             std::this_thread::sleep_for(std::chrono::seconds(3));
             std::cout << "Function c is running\n";
-        }
-        << []()
+        } << []()
         {
             std::cout << "Function d is running\n";
         };
     };
 
-    go >> []()
+    go << []()
     {
         std::cout << "Function e is running\n";
     };
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
     return 0;
 }
