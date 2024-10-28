dcab17d9690726293c0bb871e91a2d3887d18151
MRB <mehrabshafae@gmail.com>
2024-10-28 10:20:54 +0330

    mrb: uv test 3 (leaks)

--- test/go-uv.cc
+++ test/go-uv.cc
@@ -1,53 +1,68 @@
 #include <iostream>
 #include <uv.h>
-#include <thread> // Include this header for std::this_thread
+#include <thread>
 
-class TaskExecutor {
+class TaskExecutor
+{
 public:
-    TaskExecutor() {
-        // Initialize libuv loop
+    TaskExecutor()
+    {
         loop = uv_default_loop();
     }
 
     template <typename Func>
-    void execute(Func&& func) {
-        uv_work_t* req = new uv_work_t;
-        req->data = new Func(std::forward<Func>(func)); // Store the function
+    void execute(Func &&func)
+    {
+        uv_work_t *req = new uv_work_t;
+        req->data = new Func(std::forward<Func>(func));
 
-        // Queue the work
-        uv_queue_work(loop, req, [](uv_work_t* req) {
-            // This function runs in the thread pool
+        uv_queue_work(loop, req, [](uv_work_t *req)
+                      {
+           
             auto func = static_cast<Func*>(req->data);
-            (*func)(); // Call the task
-            delete func; // Clean up the function
-        }, [](uv_work_t* req, int) {
-            // This function runs in the main loop after the task is done
-            delete req; // Clean up the request
-        });
+            (*func)();
+            delete func; }, [](uv_work_t *req, int)
+                      { delete req; });
 
-        // Run the loop immediately to execute tasks
         uv_run(loop, UV_RUN_NOWAIT);
     }
 
+    void cleanup()
+    {
+        uv_run(loop, UV_RUN_DEFAULT);
+        uv_loop_close(loop);
+    }
+
+    ~TaskExecutor()
+    {
+
+        cleanup();
+    }
+
 private:
-    uv_loop_t* loop;
+    uv_loop_t *loop;
 };
 
 TaskExecutor executor;
 
-int main() {
+int main()
+{
+    const size_t task_count = 100000;
 
-    for (size_t i = 0; i < 100000; i++)
+    for (size_t i = 0; i < task_count; i++)
     {
-        executor.execute([]() {
-            executor.execute([]() {
-                executor.execute([]() {});
+        executor.execute([=]()
+                         {
+            executor.execute([=]() {
+               
             });
-            executor.execute([]() {});
-        });
+            executor.execute([=]() {
+               
+            });
+            });
     }
 
-    std::cout << "Function e is running\n";
+    std::cout << "All tasks submitted.\n";
 
     return 0;
 }
