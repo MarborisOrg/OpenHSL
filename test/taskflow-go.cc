90d0b611dc65e7ee6ff211af60a69ebb53776a60
MRB <mehrabshafae@gmail.com>
2024-10-29 00:36:33 +0330

    mrb: testing 30 million nested tasks with TaskFlow (No Crash) -> OK

--- test/taskflow-go.cc
+++ test/taskflow-go.cc
@@ -1,28 +1,17 @@
 #include <taskflow/taskflow.hpp>
 
+#define lm []()
+#define esa executor.silent_async
+
 tf::Executor executor;
 
 int main()
 {
 
-    // create asynchronous tasks directly from an executor
-    // std::future<int> future = executor.async([](){
-    // std::cout << "async task returns 1\n";
-    // return 1;
-    // });
-
-    for (size_t i = 0; i < 1000000; i++)
+    for (size_t i = 0; i < 30000000; i++)
     {
-        executor.silent_async([]()
-                              { executor.silent_async([]()
-                                                      { executor.silent_async([]() {}); }); });
+        esa(lm { esa(lm { esa(lm { esa(lm { esa(lm { esa(lm{ }); }); }); }); }); });
     }
-
-    // create asynchronous tasks with dynamic dependencies
-    // tf::AsyncTask A = executor.silent_dependent_async([](){ printf("A\n"); });
-    // tf::AsyncTask B = executor.silent_dependent_async([](){ printf("B\n"); }, A);
-    // tf::AsyncTask C = executor.silent_dependent_async([](){ printf("C\n"); }, A);
-    // tf::AsyncTask D = executor.silent_dependent_async([](){ printf("D\n"); }, B, C);
 
     executor.wait_for_all();
     return 0;
