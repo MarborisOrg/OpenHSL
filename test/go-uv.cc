d7b5c6b896adc31c51ea09fe75adf83b84e405ec
MRB <mehrabshafae@gmail.com>
2024-10-28 09:49:29 +0330

    mrb: uv test

--- test/go-uv.cc
+++ test/go-uv.cc
@@ -1,52 +1,66 @@
 #include <iostream>
 #include <uv.h>
-#include <functional>
+#include <chrono>
 
-class uvTaskClass
-{
+class TaskExecutor {
 public:
-    uvTaskClass& operator<<(std::function<void()> func)
-    {
-        addTask(func);
-        return *this;
+    TaskExecutor() {
+        // Initialize libuv loop
+        loop = uv_default_loop();
     }
 
-    void addTask(std::function<void()> func)
-    {
+    template <typename Func>
+    void execute(Func&& func) {
         uv_work_t* req = new uv_work_t;
-        req->data = new std::function<void()>(func);
-        uv_queue_work(uv_default_loop(), req, [](uv_work_t* req) {
-            auto func = static_cast<std::function<void()>*>(req->data);
-            (*func)();
-            delete func;
-        }, [](uv_work_t* req, int status) {
-            delete req;
+        req->data = new Func(std::forward<Func>(func)); // Store the function
+
+        // Queue the work
+        uv_queue_work(loop, req, [](uv_work_t* req) {
+            // This function runs in the thread pool
+            auto func = static_cast<Func*>(req->data);
+            (*func)(); // Call the task
+            delete func; // Clean up the function
+        }, [](uv_work_t* req, int) {
+            // This function runs in the main loop after the task is done
+            delete req; // Clean up the request
         });
+
+        // Run the loop immediately to execute tasks
+        uv_run(loop, UV_RUN_NOWAIT);
     }
+
+private:
+    uv_loop_t* loop;
 };
 
-uvTaskClass go;
+TaskExecutor executor;
 
-int main()
-{
-    go << []() {
-        std::cout << "Function top is running\n";
-        go << []() {
-            std::cout << "Function a is running\n";
-        } << []() {
-            std::cout << "Function b is running\n";
-        } << []() {
-            std::cout << "Function c is running\n";
-        } << []() {
-            std::cout << "Function d is running\n";
-        };
-        std::cout << "Function end is running\n";
-    };
+int main() {
+    executor.execute([]() {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "Function a is running\n";
+    });
 
-    go << []() {
+    executor.execute([]() {
+        std::cout << "Function b is running\n";
+        // std::this_thread::sleep_for(std::chrono::seconds(5));
+    });
+
+    executor.execute([]() {
+        // std::this_thread::sleep_for(std::chrono::seconds(3));
+        std::cout << "Function c is running\n";
+    });
+
+    executor.execute([]() {
+        std::cout << "Function d is running\n";
+    });
+
+    executor.execute([]() {
         std::cout << "Function e is running\n";
-    };
+    });
 
-    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+    // Keep the main thread alive long enough for the tasks to complete
+    // std::this_thread::sleep_for(std::chrono::seconds(10));
+
     return 0;
 }
