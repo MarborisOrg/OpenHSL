0b6422b86140063cff278b9acce588a551004b79
MRB <mehrabshafae@gmail.com>
2024-10-28 20:20:31 +0330

    mrb: taskflow (nested testing -> No leaks, No Crash) -> OK

--- test/taskflow-go.cc
+++ test/taskflow-go.cc
@@ -1,11 +1,29 @@
 #include <taskflow/taskflow.hpp>
 
-int main() {
-    tf::Taskflow taskflow;
-    tf::Executor executor;
+tf::Executor executor;
 
-    taskflow.emplace([]() { std::cout << "Hello, Taskflow!" << std::endl; });
+int main()
+{
 
-    executor.run(taskflow).wait();
+    // create asynchronous tasks directly from an executor
+    // std::future<int> future = executor.async([](){
+    // std::cout << "async task returns 1\n";
+    // return 1;
+    // });
+
+    for (size_t i = 0; i < 1000000; i++)
+    {
+        executor.silent_async([]()
+                              { executor.silent_async([]()
+                                                      { executor.silent_async([]() {}); }); });
+    }
+
+    // create asynchronous tasks with dynamic dependencies
+    // tf::AsyncTask A = executor.silent_dependent_async([](){ printf("A\n"); });
+    // tf::AsyncTask B = executor.silent_dependent_async([](){ printf("B\n"); }, A);
+    // tf::AsyncTask C = executor.silent_dependent_async([](){ printf("C\n"); }, A);
+    // tf::AsyncTask D = executor.silent_dependent_async([](){ printf("D\n"); }, B, C);
+
+    executor.wait_for_all();
     return 0;
 }
